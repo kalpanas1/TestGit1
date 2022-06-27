328630f706361f01a857e7ef589bd36a06da7add
kasiddap <kalpana.siddappa@capgemini.com>
2022-06-27 10:57:47 +0200

    initial commit3

--- src/main/java/TestGitMail.java
+++ src/main/java/TestGitMail.java
@@ -1,5 +1,5 @@
 public class TestGitMail {
     public static void main(String []args){
-        System.out.println("Hello Git ,----------change 2");
+        System.out.println("Hello Git ,----------change 3");
     }
 }
